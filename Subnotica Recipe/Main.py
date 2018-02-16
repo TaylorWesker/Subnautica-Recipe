b4b005f180b956a7ddfdb9749ac45988aca94727
TaylorWesker <36548825+TaylorWesker@users.noreply.github.com>
2018-02-16 20:32:16 +0100

    Add files via upload

---
+++ Subnotica Recipe/Main.py
@@ -0,0 +1,12 @@
+from View import View
+from tkinter import *
+from DBManager import *
+
+def main():
+    master = Tk()
+    View(master)
+    mainloop()
+
+
+if __name__ == '__main__':
+    main()
